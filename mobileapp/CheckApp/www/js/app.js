ca2b9a5a1b495494ef854ad5099e2c073e3c54d3
Rikin Patel <rikin-patel@live.in>
2016-08-27 10:27:09 +0530

    UI Completed with page linked

--- mobileapp/CheckApp/www/js/app.js
+++ mobileapp/CheckApp/www/js/app.js
@@ -3,22 +3,47 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('app', {
+    url: '/app',
+    templateUrl: 'templates/first.html'
+  })
+  
+   .state('groups', {
+      url: '/groups',
+      templateUrl: 'templates/groups.html'
+    })
+	
+	.state('search', {
+      url: '/search',
+      templateUrl: 'templates/search.html'
+    })
+	
+	.state('recent', {
+      url: '/recent',
+      templateUrl: 'templates/recent_groups.html'
+    })
+	
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app');
+});
